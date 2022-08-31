9d44a6ebd846e0d18516b162e5261a7375910473
Trihilman <trihilmannur14@gmail.com>
2022-08-31 13:34:31 +0700

    menambahkan view binding

--- app/src/main/java/com/zamzam/helloworld/MainActivity.kt
+++ app/src/main/java/com/zamzam/helloworld/MainActivity.kt
@@ -2,10 +2,22 @@ package com.zamzam.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.zamzam.helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnHitung.setOnClickListener { hitung() }
+    }
+
+    private fun hitung() {
+
     }
 }
