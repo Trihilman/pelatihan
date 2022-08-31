ead1ebb81e67b3aea9eabbb0a3273be104ae725f
Trihilman <trihilmannur14@gmail.com>
2022-08-31 14:16:41 +0700

    Menghitung BMI dan Kategorinya

--- app/src/main/java/com/zamzam/helloworld/MainActivity.kt
+++ app/src/main/java/com/zamzam/helloworld/MainActivity.kt
@@ -2,7 +2,6 @@ package com.zamzam.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.zamzam.helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +17,33 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun hitung() {
+        val berat = binding.etBerat.text.toString().toFloat()
+        val tinggi = binding.etTinggi.text.toString().toFloat() / 100
+        val bmi = berat / (tinggi * tinggi)
 
+        val selectedId = binding.radioGroup.checkedRadioButtonId
+        val isMale = selectedId == R.id.rbPria
+
+        val kategori = getKategori(bmi, isMale)
+        binding.tvBMI.text = getString(R.string.bmi_x, bmi)
+        binding.tvKategori.text = getString(R.string.kategori_x, kategori)
+    }
+
+    private fun getKategori(bmi: Float, isMale: Boolean): String {
+        val stringRes = if (isMale) {
+            when {
+                bmi < 20.5 -> R.string.kurus
+                bmi >= 27.0 -> R.string.gemuk
+                else -> R.string.ideal
+            }
+        }
+        else {
+            when {
+                bmi < 18.5 -> R.string.kurus
+                bmi >= 25.0 -> R.string.gemuk
+                else -> R.string.ideal
+            }
+        }
+        return getString(stringRes)
     }
 }
