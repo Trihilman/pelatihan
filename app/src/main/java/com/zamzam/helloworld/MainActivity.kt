ed0636fdcdb32f82331a88aaf665da34cc2c1dfa
Trihilman <trihilmannur14@gmail.com>
2022-09-01 09:21:14 +0700

    membuat layout list dengan RecyclerView

--- app/src/main/java/com/zamzam/helloworld/MainActivity.kt
+++ app/src/main/java/com/zamzam/helloworld/MainActivity.kt
@@ -2,23 +2,12 @@ package com.zamzam.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var imageView: ImageView
-    private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        imageView = findViewById(R.id.imgHewan)
-        textView = findViewById(R.id.tvHewan)
-        val button: Button = findViewById(R.id.btnLanjut)
 
         val dataHewan = ArrayList<Hewan>().apply {
             add(Hewan("Ayam", R.drawable.ayam))
@@ -27,16 +16,5 @@ class MainActivity : AppCompatActivity() {
             add(Hewan("Kambing", R.drawable.kambing))
             add(Hewan("Sapi", R.drawable.sapi))
         }
-        showData(dataHewan[0])
-        var count = 0
-        button.setOnClickListener {
-            count = (count + 1) % dataHewan.size
-            showData(dataHewan[count])
-        }
-    }
-
-    private fun showData(hewan: Hewan) {
-        imageView.setImageResource(hewan.gambarResId)
-        textView.text = hewan.nama
     }
 }
