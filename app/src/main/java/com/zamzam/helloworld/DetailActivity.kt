a40fda719a46389fea12f18e786c033639e7b933
Trihilman <trihilmannur14@gmail.com>
2022-09-02 10:25:44 +0700

    menambahkan menu simpan pada detailActivity

--- app/src/main/java/com/zamzam/helloworld/DetailActivity.kt
+++ app/src/main/java/com/zamzam/helloworld/DetailActivity.kt
@@ -2,8 +2,9 @@ package com.zamzam.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.zamzam.helloworld.databinding.ActivityDetailBinding
-import com.zamzam.helloworld.databinding.ActivityMainBinding
 
 class DetailActivity : AppCompatActivity() {
 
@@ -13,5 +14,18 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_detail, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menuSimpan){
+            finish()
+            return true
+        }
+        return false
     }
 }
